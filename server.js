646890e0b2b98fe58ccb873dd8ab69cb3bdad91e
daltonhahn <hahnd11@gmail.com>
2017-03-27 22:38:55 -0500

    Got database to connect for first time

--- server.js
+++ server.js
@@ -11,3 +11,22 @@ app.use(express.static('web/public'));
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
+
+var pgp = require('pg-promise')();
+var cn = {
+  host: 'localhost',
+  port: 5432,
+  database: 'kspact',
+  user: 'kspact',
+  password: 'kspact'
+};
+
+var db = pgp(cn);
+db.connect()
+    .then(function (obj) {
+        obj.done(); // success, release the connection;
+        console.log("successful connection to db");
+    })
+    .catch(function (error) {
+        console.log("ERROR:", error.message || error);
+    });

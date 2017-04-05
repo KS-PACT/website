6e47f831c68ed83f9014205c0fc7c928a6c9d96e
Joy Hauser <Joy Hauser>
2017-04-05 16:23:36 -0500

    Add test data for web users

---
+++ web/db/AddTestData.sql
@@ -0,0 +1,70 @@
+/* User test data */
+INSERT INTO WebUser (
+	id,
+	first_name,
+	last_name,
+	username,
+    password,
+	email,
+	school,
+    bio,
+    picture,
+    grade_level,
+	privilege) VALUES (
+	'Josh',
+	'Weese',
+	'weeser',
+	'1234',
+	'weeser@ksu.edu',
+	'Kansas State University',
+	NULL,
+	'blah',
+	'3rd',
+	'Admin'
+	);
+INSERT INTO WebUser (
+	id,
+	first_name,
+	last_name,
+	username,
+    password,
+	email,
+	school,
+    bio,
+    picture,
+    grade_level,
+	privilege) VALUES (
+	'Nathan',
+	'Bean',
+	'nbean',
+	'password',
+	'nbean@ksu.edu',
+	'K-State',
+	NULL,
+	'Hello World',
+	'5th',
+	'Member'
+	);
+INSERT INTO WebUser (
+	id,
+	first_name,
+	last_name,
+	username,
+    password,
+	email,
+	school,
+    bio,
+    picture,
+    grade_level,
+	privilege) VALUES (
+	'Blahahahahahahahahahahahahahahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh',
+	'De Blahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh',
+	'bdeblahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh',
+	'passwordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpassword',
+	'bdeblahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh@ksu.edu',
+	'Kansas Missouri Mississippi Colorado Hawaii Alaska California Maine Nebraska Arizona',
+	NULL,
+	'All of the wordssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss',
+	'College Prep',
+	'Admin'
+	);

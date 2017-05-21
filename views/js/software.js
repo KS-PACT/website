1238c9da6a3cf72645d4bd5628b64a4f797489ee
Joy Hauser <Joy Hauser>
2017-05-21 18:37:05 -0500

    Add software edit modal

--- views/js/software.js
+++ views/js/software.js
@@ -1,4 +1,28 @@
-// Send data to server functions
+// Add software element to database
+function addSoftware() {
+	var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+
+	$.ajax({
+		type: 'post',
+		url: '/software',
+		data: { 'action': 'add', 'name': $("#addName").val(), 'description': $("#addDescription").val(), 'link': $("#addLink").val(), 'color': color },
+		dataType: "json",
+		success: function (data) {
+			console.log("Status: ", data.status);
+			if(data.status == "Success") {
+				bootstrap_alert.success('You correctly added a software resource.');
+			}
+			else {
+				bootstrap_alert.error(data.status);
+			}
+		},
+		error: function (xhr, status, err) {
+			console.error('text status '+status+', err '+err);
+		}
+	});
+}
+
+// Remove software element from database
 function removeSoftware(id) {
 	$.ajax({
 		type: 'post',
@@ -20,6 +44,58 @@ function removeSoftware(id) {
 	});
 }
 
+// Get user information from server
+function getSoftwareInfo(id) {
+	$.ajax({
+		type: 'post',
+		url: '/software',
+		data: { 'action': 'get info', 'id': id },
+		dataType: "json",
+		success: function (data) {
+			$("#editName").val(data.info[0].name);
+			$("#editDescription").val(data.info[0].description);
+			$("#editLink").val(data.info[0].link);
+		},
+		error: function (xhr, status, err) {
+			console.error('text status '+status+', err '+err);
+		}
+	});
+}
+
+// Update hardware on server
+function updateSoftware() {
+	$.ajax({
+		type: 'post',
+		url: '/software',
+		data: { 'action': 'update',
+						'name': $('#editName').val(),
+						'description': $('#editDescription').val(),
+						'link': $('#editLink').val() },
+		dataType: "json",
+		success: function (data) {
+			console.log(data.status);
+			location.reload();
+		},
+		error: function (xhr, status, err) {
+			console.error('text status '+status+', err '+err);
+		}
+	});
+}
+
+// Enable all fields on edit modal
+function enableEditModalFields() {
+	$('#editName').prop('disabled', false);
+	$('#editDescription').prop('disabled', false);
+	$('#editLink').prop('disabled', false);
+}
+
+// Disable all fields on edit modal
+function disableEditModalFields() {
+	$('#editName').prop('disabled', true);
+	$('#editDescription').prop('disabled', true);
+	$('#editLink').prop('disabled', true);
+}
+
 // Handle notifications
 bootstrap_alert = function() {}
 bootstrap_alert.success = function(message) {
@@ -36,4 +112,24 @@ $('.remove-btn').on('click', function() {
 	if(event.stopPropagation) event.stopPropagation();
 	
 	removeSoftware($(this).data("id"));
+});
+
+$('.add-software-card').on('click', function() {
+	$('#addSoftwareModal').modal('show');
+});
+
+$('.add-submit-btn').on('click', function() {
+	addSoftware();
+});
+
+$('.link-btn').on('click', function() {
+	location.href = $("#editLink").val();
+});
+
+$('.edit-software-card').on('click', function() {
+	getSoftwareInfo($(this).data("id"));
+	
+	disableEditModalFields();
+	
+	$('#editSoftwareModal').modal('show');
 });

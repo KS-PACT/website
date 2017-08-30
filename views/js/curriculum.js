00ec5e657f3415b68f4653f46e24aca810379597
Joy Hauser <joy.hauser@hotmail.com>
2017-08-29 19:15:40 -0500

    Add filtering for the curriculums page

--- views/js/curriculum.js
+++ views/js/curriculum.js
@@ -1,3 +1,19 @@
+function filterCurriculums() {
+    var input = document.getElementById('searchField');
+    var filter = input.value.toUpperCase();
+
+	$( ".edit-curriculum-card" ).each(function() {
+		var cardContent = $( this ).context;
+		var cardName = cardContent.getElementsByTagName("h3")[0].innerHTML;
+
+		if (cardName.toUpperCase().indexOf(filter) > -1) {
+            cardContent.style.display = "";
+        } else {
+            cardContent.style.display = "none";
+        }
+	});
+}
+
 // Remove curriculum element from DB
 function removeCurriculum(id) {
 	$.ajax({
